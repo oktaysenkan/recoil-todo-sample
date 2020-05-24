41b0c657552ff7fff877e1d857feefe5f83384a3
Oktay Şenkan <oktay.senkan@gmail.com>
2020-05-24 14:06:26 +0300

    implemented drag and drop

--- src/App.js
+++ src/App.js
@@ -22,50 +22,110 @@ const App = () => {
     setTodoName(event.target.value);
   };
 
-  const handleAddTodo = () => {
-    setTodos(
-      todos.concat({
-        name: todoName,
-        status: 'todo',
-      })
-    );
+  const handleAddTodo = (name = todoName, type) => {
+    if (type === 'todo') {
+      setTodos(
+        todos.concat({
+          name,
+          status: type,
+        })
+      );
+      return;
+    }
+
+    if (type === 'inprogress') {
+      setInProgress(
+        inProgress.concat({
+          name,
+          status: type,
+        })
+      );
+      return;
+    }
+
+    if (type === 'done') {
+      setDone(
+        done.concat({
+          name,
+          status: type,
+        })
+      );
+      return;
+    }
   };
 
-  const handleAddInProgress = () => {
-    setInProgress(
-      inProgress.concat({
-        name: todoName,
-        status: 'todo',
-      })
-    );
+  const removeTodo = (name) => {
+    setTodos(todos.filter((item) => item.name !== name));
+    setInProgress(inProgress.filter((item) => item.name !== name));
+    setDone(done.filter((item) => item.name !== name));
   };
 
-  const handleAddDone = () => {
-    setDone(
-      done.concat({
-        name: todoName,
-        status: 'done',
-      })
-    );
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e, type) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const todoName = e.dataTransfer.getData('name');
+
+    if (!todoName) {
+      return;
+    }
+
+    removeTodo(todoName);
+    handleAddTodo(todoName, type);
   };
 
   return (
     <div className="app">
-      <div>
+      <div
+        className="todo"
+        onDrop={(e) => handleDrop(e, 'todo')}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
         <input type="text" onChange={handleTodoNameChange} />
-        <button onClick={handleAddTodo}>Add</button>
+        <button onClick={() => handleAddTodo(todoName, 'todo')}>Add</button>
         <List data={todos} />
       </div>
 
-      <div>
+      <div
+        className="in-progress"
+        onDrop={(e) => handleDrop(e, 'inprogress')}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
         <input type="text" onChange={handleTodoNameChange} />
-        <button onClick={handleAddInProgress}>Add</button>
+        <button onClick={() => handleAddTodo(todoName, 'inprogress')}>
+          Add
+        </button>
         <List data={inProgress} />
       </div>
 
-      <div>
+      <div
+        className="done"
+        onDrop={(e) => handleDrop(e, 'done')}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
         <input type="text" onChange={handleTodoNameChange} />
-        <button onClick={handleAddDone}>Add</button>
+        <button onClick={() => handleAddTodo(todoName, 'done')}>Add</button>
         <List data={done} />
       </div>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const List = ({ data }) => {
+  const onDragStart = (event, name) => {
+    event.dataTransfer.setData('name', name);
+  };
+
   return (
     <div>
       {data.map((item, i) => (
-        <div key={i}>
+        <div key={i} draggable onDragStart={(e) => onDragStart(e, item.name)}>
           <h3>{item.name}</h3>
           {item.status}
         </div>
